63481ffd148efd343d79c7a7311f14e2c0d97262
Soumit Salman Rahman <soumitsr@gmail.com>
2025-06-11 15:16:04 -0400

    making agent clients a bit more generic

--- foundry/converter.py
+++ foundry/converter.py
@@ -1,21 +1,24 @@
 import os
 
-def convert_to_onnx(model_id: str, save_directory):
-    from optimum.onnxruntime import ORTModelForFeatureExtraction, ORTQuantizer, AutoQuantizationConfig
-    from transformers import AutoTokenizer
 
-    model = ORTModelForFeatureExtraction.from_pretrained(model_id, export=True, cache_dir=os.getenv('HF_HOME'))
+
+def convert_to_onnx(model_id: str, save_directory: str, model_class):
+    from optimum.onnxruntime import ORTQuantizer, AutoQuantizationConfig
+    from transformers import AutoTokenizer
+    import torch
+
+    model = model_class.from_pretrained(model_id, export=True, cache_dir=os.getenv('HF_HOME'))
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     os.makedirs(save_directory, exist_ok=True)
   
-    quantizer = ORTQuantizer.from_pretrained(model)
-    quantizer.quantize(save_dir=save_directory, quantization_config=AutoQuantizationConfig.avx512(is_static=False))
+    quantizer = ORTQuantizer.from_pretrained(model, file_name="model_int8.onnx")
+    q_config = AutoQuantizationConfig.avx2(is_static=False)
+    quantizer.quantize(save_dir=save_directory, quantization_config=q_config)
     tokenizer.save_pretrained(save_directory)
 
 def convert_to_openvino(model_id: str, save_directory: str, model_class):
     from optimum.intel.openvino.quantization import OVQuantizer
     from transformers import AutoTokenizer
-    from datasets import load_dataset
 
     # Load the model and tokenizer
     model = model_class.from_pretrained(model_id, export=True, cache_dir=os.getenv('HF_HOME'))
@@ -28,11 +31,9 @@ def convert_to_openvino(model_id: str, save_directory: str, model_class):
     tokenizer.save_pretrained(save_directory)
 
 if __name__ == "__main__":
-    from optimum.intel.openvino import OVModelForFeatureExtraction, OVModelForCausalLM, OVModelForSeq2SeqLM
-    models = ["HuggingFaceTB/SmolLM2-135M-Instruct", "HuggingFaceTB/SmolLM2-360M-Instruct", "google/long-t5-tglobal-base", "google/long-t5-local-base"]
-    model_classes = [OVModelForCausalLM, OVModelForCausalLM, OVModelForSeq2SeqLM, OVModelForSeq2SeqLM]
-    for path, mc in zip(models, model_classes):
-        convert_to_openvino(path, ".models/"+path.split("/")[-1]+"-openvino", mc)
+    # from optimum.intel.openvino import OVModelForFeatureExtraction, OVModelForCausalLM, OVModelForSeq2SeqLM
+    path = "/home/soumitsr/codes/pycoffeemaker/.models/long-t5-local-base-onnx"
+    # convert_to_onnx_t5(path, "/home/soumitsr/codes/pycoffeemaker/.models/"+path.split("/")[-1]+"-onnx")
     # convert_to_openvino("soumitsr/SmolLM2-360M-Instruct-article-digestor", "models/smollm2-360M-instruct-article-digestor-ovquant")
     # with open("embedder-test-data.json", "r") as file:
     #     data = json.load(file)
