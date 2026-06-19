9b71e5616c7fb8c751d35c3cabba51e8c64c1127
Soumit Salman Rahman <soumitsr@gmail.com>
2026-06-19 11:08:13 -0400

    minor change in default params

--- analysts.py
+++ analysts.py
@@ -249,10 +249,9 @@ class VLLMTextAnalyst(TextAnalystBase):
                 max_model_len=self.context_len,
                 max_num_seqs=VLLM_MAX_NUM_SEQS if VLLM_MAX_NUM_SEQS > 0 else None,
                 max_num_batched_tokens=VLLM_MAX_NUM_BATCHED_TOKENS if VLLM_MAX_NUM_BATCHED_TOKENS > 0 else None,
-                gpu_memory_utilization=VLLM_GPU_MEMORY_UTILIZATION if VLLM_GPU_MEMORY_UTILIZATION > 0 else None,
+                gpu_memory_utilization=VLLM_GPU_MEMORY_UTILIZATION,
                 enable_prefix_caching=True,
-                enable_chunked_prefill=True,
-                # language_model_only=True,             
+                enable_chunked_prefill=True,        
                 trust_remote_code=True,
                 attention_config={"backend": VLLM_ATTENTION_BACKEND} if VLLM_ATTENTION_BACKEND else None,
             )
